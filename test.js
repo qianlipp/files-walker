01ec1b30dd9826af3b58a3b9ff7010adf8492e83
qianlipp <yuqianliwb@gmail.com>
2017-06-22 22:30:08 +0800

    init

--- test.js
+++ test.js
@@ -6,4 +6,13 @@ var index = require("./lib/index");
 
 index.walkSync("/Users/vinceyu/projects/我的公开项目", function (path, name) {
     console.log(path + "__________" + name);
+}, {
+    contain: "symbol",
+    not_contain: "@core-js",
+    start_with: "/Users/vi",
+    not_start_with: "/",
+    end_with: "",
+    not_end_with: "",
+    filter: function () {
+    }
 });
