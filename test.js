c2feaa708c12bed9d7ff130c37f600ecdc28b008
qianlipp <yuqianliwb@gmail.com>
2017-06-22 22:16:32 +0800

    init

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 /**
  * Created by qianlipp on 2017/6/16.
  */
-var utils = require("./lib/utils");
 
-utils.find("/Users/vinceyu/projects/我的公开项目",function () {
+var index = require("./lib/index");
 
+index.walkSync("/Users/vinceyu/projects/我的公开项目", function (path, name) {
+    console.log(path + "__________" + name);
 });
